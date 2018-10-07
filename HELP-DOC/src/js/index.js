326bcfac359b6413474455d121642ed07d172127
chenqifu <2371400356@qq.com>
2018-10-07 19:09:45 +0800

    'chenqifu:second'

---
+++ HELP-DOC/src/js/index.js
@@ -0,0 +1,65 @@
+$('.down').on('click',function(event){
+    event.preventDefault();
+    $('.d_p').toggle();
+    $('.up').toggle();
+    $('.down').toggle();
+});
+$('.up').on('click',function(event){
+    event.preventDefault();
+    $('.d_p').toggle();
+    $('.up').toggle();
+    $('.down').toggle();
+});
+// =======================================================
+$('.down_2').on('click',function(event){
+    event.preventDefault();
+    $('.d_p_2').toggle();
+    $('.up_2').toggle();
+    $('.down_2').toggle();
+});
+$('.up_2').on('click',function(event){
+    event.preventDefault();
+    $('.d_p_2').toggle();
+    $('.up_2').toggle();
+    $('.down_2').toggle();
+});
+// =================================================
+$('.down_3').on('click',function(event){
+    event.preventDefault();
+    $('.d_p_3').toggle();
+    $('.up_3').toggle();
+    $('.down_3').toggle();
+});
+$('.up_3').on('click',function(event){
+    event.preventDefault();
+    $('.d_p_3').toggle();
+    $('.up_3').toggle();
+    $('.down_3').toggle();
+});
+// ====================================
+$('.down_4').on('click',function(event){
+    event.preventDefault();
+    $('.d_p_4').toggle();
+    $('.up_4').toggle();
+    $('.down_4').toggle();
+});
+$('.up_4').on('click',function(event){
+    event.preventDefault();
+    $('.d_p_4').toggle();
+    $('.up_4').toggle();
+    $('.down_4').toggle();
+});
+// ================================================
+$('.down_5').on('click',function(event){
+    event.preventDefault();
+    $('.d_p_5').toggle();
+    $('.up_5').toggle();
+    $('.down_5').toggle();
+});
+$('.up_5').on('click',function(event){
+    event.preventDefault();
+    $('.d_p_5').toggle();
+    $('.up_5').toggle();
+    $('.down_5').toggle();
+});
+// ==================================================
